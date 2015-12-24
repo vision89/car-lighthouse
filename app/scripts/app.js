6f150131c3149008fe5c8a970f40586e1078e04a
Dustin Gulley <dustingulley@Dustin-Gulleys-MacBook-Pro.local>
2015-12-23 21:07:18 -0500

    Updated to use local storage instead of firebase

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,21 +14,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  app.user = null;
-  app.carDocumentUrl = '';
-  app.noteDocumentUrl = '';
-  app.noteContent = '';
-  app.viewMode = true;
-  app.distance = 0;
-  app.carLocationSet = false;
-  app.showError = false;
-  app.isMobile = false;
-
-  app.parkingDescription = {
-
-    description: ''
-
-  };
 
   app.displayInstalledToast = function() {
 
@@ -36,18 +21,47 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
   };
 
+  /**
+   * Set up the default data for local storage
+   * @return {[type]} [description]
+   */
+  app.initializeDefaultData = function() {
+
+    app.set( 'carData', {
+
+      viewMode: true,
+      carLocation: null,
+      showError: false,
+      isMobile: false,
+      noteContent: ''
+
+    });
+
+  };
+
+  /**
+   * Check the data
+   * @return {[type]} [description]
+   */
+  app.initializeData = function () {
+
+    if ( app.carData === undefined || app.carData === null ) {
+
+      //app.initializeDefaultData();
+
+    }
+
+  };
+
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
 
-    app.firebaseUrl = 'https://car-lighthouse.firebaseio.com';
-    app.carDocumentUrl = app.firebaseUrl;
-    app.noteDocumentUrl = app.firebaseUrl;
-    app.signedIn = false;
+    app.distance = 0;
 
     //Detect if were on a mobile browser to decrease ad size
     if( /Android|webOS|iPhone|iPod|BlackBerry/i.test( navigator.userAgent ) ) {
 
-      app.isMobile = true;
+      app.set( 'carData.isMobile', true );
 
     }
 
@@ -83,9 +97,9 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
       setTimeout( function () {
 
-        if ( app.user !== undefined && app.user !== null && app.coords ) {
+        if ( app.coords ) {
 
-          _computeDistance( { 'lat': app.coords.latitude, 'lng': app.coords.longitude }, app.carLocation );
+          _computeDistance( { 'lat': app.coords.latitude, 'lng': app.coords.longitude }, app.carData.carLocation );
 
           _computeDistanceLoop( 5000 );
 
@@ -95,23 +109,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
     }
 
-    /**
-     * Sign in button clicked
-     * @return {[type]} [description]
-     */
-    app.signInClicked = function () {
-
-      if ( app.signedIn === false ) {
-
-        app.$.firebaseLogin.login();
-
-      } else {
-
-        this.$.firebaseLogin.logout();
-
-      }
-
-    };
 
     /**
      * Update car location
@@ -119,14 +116,14 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
      */
     app.updateCarLocation = function () {
 
-      app.carLocation = {
+      app.set( 'carData.carLocation', {
 
         'lat': app.coords.latitude,
         'lng': app.coords.longitude
 
-      };
+      });
 
-      _computeDistance( { 'lat': app.coords.latitude, 'lng': app.coords.longitude }, app.carLocation );
+      _computeDistance( { 'lat': app.coords.latitude, 'lng': app.coords.longitude }, app.carData.carLocation );
 
     };
 
@@ -136,17 +133,17 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
      */
     app.switchMode = function () {
 
-      if ( app.viewMode === false ) {
+      if ( app.carData.viewMode === false ) {
 
         app.note = {
 
-          content: app.noteContent
+          content: app.carData.noteContent
 
         };
 
       }
 
-      app.viewMode = !app.viewMode;
+      app.set( 'carData.viewMode', !app.carData.viewMode );
 
     };
 
@@ -156,18 +153,18 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
      */
     app.getDirections = function () {
 
-      if ( app.carLocationSet === true && app.coords ) {
+      if ( app.carData.carLocation && app.coords ) {
 
-        var url = 'http://maps.google.com/maps?saddr=' + app.coords.latitude + ',' + app.coords.longitude + '&daddr=' + app.carLocation.lat + ',' + app.carLocation.lng + '&mode=walking';
+        var url = 'http://maps.google.com/maps?saddr=' + app.coords.latitude + ',' + app.coords.longitude + '&daddr=' + app.carData.carLocation.lat + ',' + app.carData.carLocation.lng + '&mode=walking';
 
         var win = window.open(url, '_blank');
         win.focus();
 
-        app.showError = false;
+        app.set( 'carData.showError', false );
 
       } else {
 
-        app.showError = true;
+        app.set( 'carData.showError', true );
 
       }
 
@@ -180,17 +177,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
       if ( app.note !== undefined && app.note !== null ) {
 
-        app.noteContent = app.note.content;
-
-      }
-
-    };
-
-    app.setCarLocation = function () {
-
-      if ( app.carLocation !== undefined && app.carLocation !== null ) {
-
-        app.carLocationSet = true;
+        app.set( 'carData.noteContent', app.note.content );
 
       }
 
@@ -206,11 +193,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
      * User has succesfully logged in
      * @return {[type]} [description]
      */
-    app.loggedIn = function () {
-
-      app.signedIn = true;
-      app.carDocumentUrl = app.firebaseUrl + '/' + app.user.uid + '/car';
-      app.noteDocumentUrl = app.firebaseUrl + '/' + app.user.uid + '/note';
+    window.onload = function ( e ) {
 
       //Kick off the location updates
       setTimeout( function () {
@@ -225,20 +208,10 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
      * User has logged out
      * @return {[type]} [description]
      */
-    app.loggedOut = function () {
+    window.onbeforeunload = function ( e ) {
 
-        var locationElement = document.querySelector( '#polymer-location' );
-        locationElement.unsetWatch();
-        app.viewMode = true;
-        app.signedIn = false;
-        app.carDocumentUrl = app.firebaseUrl;
-        app.noteDocumentUrl = app.firebaseUrl;
-        app.noteContent = '';
-        app.note = {};
-        app.carLocation = {};
-        app.carLocationSet = false;
-        app.distance = 0;
-        app.showError = false;
+      var locationElement = document.querySelector( '#polymer-location' );
+      locationElement.unsetWatch();
 
     };
 
